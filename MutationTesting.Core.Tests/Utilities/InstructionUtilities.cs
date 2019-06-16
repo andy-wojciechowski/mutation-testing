64fc865d405a6d73bcf822bda41b901168e29b68
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-06-16 10:31:19 -0500

    Adding an InstructionUtilities class to help with writing unit tests

---
+++ MutationTesting.Core.Tests/Utilities/InstructionUtilities.cs
@@ -0,0 +1,26 @@
+﻿using Mono.Cecil.Cil;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MutationTesting.Core.Tests.Utilities
+{
+	/// <summary>
+	/// A collection of utilities related to IL instruction which may be helpful
+	/// when writing unit tests for a mutator
+	/// </summary>
+	internal static class InstructionUtilities
+	{
+		/// <summary>
+		/// Takes a list of IL instructions a builds a string containing those IL instructions
+		/// </summary>
+		/// <param name="instructions">a list of IL instructions</param>
+		/// <returns>a string containing all IL instructions in the given list</returns>
+		public static string ConvertInstructionsToString(IList<Instruction> instructions)
+		{
+			var sb = new StringBuilder();
+			foreach(var instruction in instructions)
+				sb.AppendLine(instruction.ToString());
+			return sb.ToString();
+		}
+	}
+}
