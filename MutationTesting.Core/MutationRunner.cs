57318f6531d3bebedc98982335c17f7c14f00514
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-06-16 12:38:06 -0500

    Started stubbing out a MutationRunner

---
+++ MutationTesting.Core/MutationRunner.cs
@@ -0,0 +1,28 @@
+﻿using MutationTesting.Core.Mutators;
+using MutationTesting.Core.ReferenceLoaders;
+using MutationTesting.Core.TestRunners;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MutationTesting.Core
+{
+	public class MutationRunner
+	{
+		private IList<IMutator> Mutators { get; }
+		private ITestRunner TestRunner { get; }
+		private IReferenceLoader ReferenceLoader { get; }
+
+		public MutationRunner(IList<IMutator> mutators, ITestRunner testRunner, IReferenceLoader referenceLoader)
+		{
+			Mutators = mutators;
+			TestRunner = testRunner;
+			ReferenceLoader = referenceLoader;
+		}
+
+		public void RunMutators()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
